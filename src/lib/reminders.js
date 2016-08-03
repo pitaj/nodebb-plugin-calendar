de4f1ececba8ca2b96ae99a676df8c6f24404fb6
Peter Jaszkowiak <p.jaszkow@gmail.com>
2016-08-02 21:26:58 -0600

    closes #49, add email notifications

--- src/lib/reminders.js
+++ src/lib/reminders.js
@@ -2,13 +2,18 @@ const notifications = require.main.require('./src/notifications');
 const posts = require.main.require('./src/posts');
 const meta = require.main.require('./src/meta');
 const user = require.main.require('./src/user');
+const emailer = require.main.require('./src/emailer');
+const translator = require.main.require('./public/src/modules/translator');
+const nconf = require.main.require('nconf');
 
 // import { fork } from 'child_process';
 import { getAll as getResponses } from './responses';
-import { getAllEvents } from './event';
+import { getAllEvents, escapeEvent } from './event';
 import { filterUidsByPid } from './privileges';
+import postTemplate from './template';
 import moment from 'moment';
 import Promise from 'bluebird';
+import { decode } from 'html-entities';
 const p = Promise.promisify;
 
 const createNotif = p(notifications.create);
@@ -16,7 +21,44 @@ const pushNotif = p(notifications.push);
 const getPostFields = p(posts.getPostFields);
 const getSetting = p(meta.settings.getOne);
 const getUidsFromSet = p(user.getUidsFromSet);
+const sendEmail = p(emailer.send);
+const getUserSettings = p(user.getSettings);
+const getUserFields = p(user.getUserFields);
+const translate = p((text, language, callback) => {
+  translator.translate(text, language, (content) => callback(null, content));
+});
 
+const emailNotification = async ({ uid, event, message, postData }) => {
+  if (parseInt(meta.config.disableEmailSubscriptions, 10) === 1) {
+    return;
+  }
+
+  const [userData, userSettings] = await Promise.all(
+    getUserFields(uid, ['username', 'userslug']),
+    getUserSettings(uid)
+  );
+
+  if (userSettings.sendPostNotifications) {
+    const parsed = await escapeEvent(event);
+    const lang = userSettings.userLang || meta.config.defaultLang;
+    const content = await translate(postTemplate(parsed), lang);
+
+    const title = decode(postData.title);
+    // const titleEscaped = title.replace(/%/g, '&#37;').replace(/,/g, '&#44;');
+
+    await sendEmail('notif_plugin_calendar_event_reminder', uid, {
+      pid: event.pid,
+      subject: `[${meta.config.title || 'NodeBB'}] ${title}`,
+      intro: `[[calendar:event_starting, ${message}, ${event.name}]]`,
+      content: content.replace(/"\/\//g, '"https://'),
+      site_title: meta.config.title || 'NodeBB',
+      username: userData.username,
+      userslug: userData.userslug,
+      url: `${nconf.get('url')}/post/${event.pid}`,
+      base_url: nconf.get('url'),
+    });
+  }
+};
 
 const notify = async ({ event, reminder, message }) => {
   let uids;
@@ -47,18 +89,26 @@ const notify = async ({ event, reminder, message }) => {
     uids = users.map((u) => u.uid);
   }
 
-  const { tid, content } = await getPostFields(event.pid, ['tid', 'content']);
-  const notif = await createNotif({
-    bodyShort: `[[calendar:event_starting, ${message}, ${event.name}]]`,
-    bodyLong: content,
-    nid: `plugin-calendar:tid:${tid}:pid:${event.pid}:event`,
-    pid: event.pid,
-    tid,
-    from: event.uid,
-    path: `/post/${event.pid}`,
-    importance: 1,
-  });
-  await pushNotif(notif, uids);
+  const postData = await getPostFields(event.pid, ['tid', 'content', 'title']);
+
+  await Promise.all(
+    createNotif({
+      bodyShort: `[[calendar:event_starting, ${message}, ${event.name}]]`,
+      bodyLong: postData.content,
+      nid: `plugin-calendar:tid:${postData.tid}:pid:${event.pid}:event`,
+      pid: event.pid,
+      tid: postData.tid,
+      from: event.uid,
+      path: `/post/${event.pid}`,
+      importance: 1,
+    }).then((notif) => pushNotif(notif, uids)),
+    ...uids.map((uid) => emailNotification({
+      event,
+      message,
+      uid,
+      postData,
+    })),
+  );
 };
 
 const initNotifierDaemon = async () => {
