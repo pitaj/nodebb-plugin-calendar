7a4aa757b4a44c6dad39632143ba378891752314
Peter Jaszkowiak <p.jaszkow@gmail.com>
2016-11-16 02:58:26 -0700

    Possible fix for unexpected token error

--- src/client/templates.js
+++ src/client/templates.js
@@ -1,11 +1,15 @@
-const eventTemplate = (event) => (
-  `[event][name]${event.name}[/name][allday]${event.allday}[/allday]` +
+const eventTemplate = (event) => {
+  const repeats = event.repeats && {
+    ...event.repeats,
+    endDate: event.repeats.endDate ? event.repeats.endDate.valueOf() : null,
+  };
+  return `[event][name]${event.name}[/name][allday]${event.allday}[/allday]` +
   `[startDate]${event.startDate}[/startDate][endDate]${event.endDate}[/endDate]` +
   `[reminders]${JSON.stringify(event.reminders)}[/reminders]` +
   `[location]${event.location}[/location]` +
   `[description]${event.description}[/description][mandatory]${event.mandatory}[/mandatory]` +
-  `${event.repeats ? `[repeats]${JSON.stringify(event.repeats)}[/repeats]` : ''}[/event]`
-);
+  `${repeats ? `[repeats]${JSON.stringify(repeats)}[/repeats]` : ''}[/event]`;
+};
 
 export {
   eventTemplate,
