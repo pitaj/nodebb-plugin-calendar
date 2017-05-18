5513238ff782ad89fcdbd3a378d544c02904e194
Peter Jaszkowiak <p.jaszkow@gmail.com>
2017-05-18 14:48:02 -0600

    NodeBB/NodeBB#5687

--- src/client/clientSideTranslation.js
+++ src/client/clientSideTranslation.js
@@ -1,27 +1,26 @@
-const translateEvents = (translate) => {
-  $('.plugin-calendar-event[data-translated=false]').each((i, elem) => {
-    const el = $(elem);
-    el.attr('data-translated', 'true');
-    translate(el.html())
-      .then((translated) => {
-        el.html(translated);
-        el.find('.plugin-calendar-time-date-view')
-          .attr('title', (x, val) => val.replace('<br>', ' | '));
-      });
-  });
-};
-
 const init = (Translator) => {
   const translator = Translator.create();
-  const translate = (text) => translator.translate(text);
+
+  const translateEvents = () => {
+    $('.plugin-calendar-event[data-translated=false]').each((i, elem) => {
+      const el = $(elem);
+      el.attr('data-translated', 'true');
+      translator.translateInPlace(elem)
+        .then(() => {
+          el.find('.plugin-calendar-time-date-view')
+            .attr('title', (x, val) => val.replace('<br>', ' | '));
+        });
+    });
+  };
+
   $(window).on([
     'action:posts.loaded',
     'action:ajaxify.end',
     'action:posts.edited',
     'action:calendar.event.display',
     'action:composer.preview',
-  ].join(' '), () => translateEvents(translate));
-  translateEvents(translate);
+  ].join(' '), () => translateEvents());
+  translateEvents();
 };
 
 export default init;
