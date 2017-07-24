bc4fe68b852f2467fe89570c9e48285a3cf69770
Florian Kinder <florian.kinder@fankserver.com>
2017-07-24 22:06:20 +0200

    Style changes

--- public/admin.js
+++ public/admin.js
@@ -1,27 +1,27 @@
 /* global $, app, define */
 /* eslint no-var: 0, prefer-arrow-callback: 0, func-names: 0 */
 
-define('admin/plugins/calendar', [], function() {
-  $('#save').on('click', function() {
+define('admin/plugins/calendar', [], function () {
+  $('#save').on('click', function () {
     var settings = {
       checkingInterval: parseInt($('#checkingInterval').val(), 10),
       mainPostOnly: !!$('#mainPostOnly')[0].checked,
       respondIfCanReply: !!$('#respondIfCanReply')[0].checked,
     };
-    $.get('/api/admin/plugins/calendar/save', { settings: JSON.stringify(settings) }, function() {
+    $.get('/api/admin/plugins/calendar/save', { settings: JSON.stringify(settings) }, function () {
       app.alertSuccess();
     });
   });
 
-  $('#addICal').on('click', function() {
-    templates.parse('partials/calendar/add-ical', {}, function(html) {
+  $('#addICal').on('click', function () {
+    templates.parse('partials/calendar/add-ical', {}, function (html) {
       var modal = bootbox.dialog({
         title: 'Add ICal',
         message: html,
         buttons: {
           create: {
             label: 'Save Response',
-            callback: function(e) {
+            callback: function (e) {
               var modal = $(e.target).parents('.modal'),
                 formEl = modal.find('form'),
                 payload = formEl.serializeObject();
@@ -33,10 +33,10 @@ define('admin/plugins/calendar', [], function() {
                 headers: {
                   'x-csrf-token': config.csrf_token
                 }
-              }).done(function() {
+              }).done(function () {
                 ajaxify.refresh();
                 modal.modal('hide');
-              }).fail(function() {
+              }).fail(function () {
                 app.alertError('Could not save ical');
               });
 
