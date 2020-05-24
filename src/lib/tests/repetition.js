f66addfb60b0b4c3253c0a240e692e7a58a7a79f
Peter Jaszkowiak <p.jaszkow@gmail.com>
2020-05-24 14:25:02 -0600

    dump dependencies

--- src/lib/tests/repetition.js
+++ src/lib/tests/repetition.js
@@ -18,7 +18,7 @@ import getOccurencesOfRepetition from '../repetition';
     1478576134000 + 5000,
     1478576134000 + (36 * 60 * 60 * 1000)
   );
-  assert.deepEqual(occurences, [
+  const expected = [
     {
       startDate: 1478576134000 + (1 * 24 * 60 * 60 * 1000),
       endDate: 1478576134000 + (1 * 24 * 60 * 60 * 1000) + (2 * 60 * 60 * 1000),
@@ -29,6 +29,9 @@ import getOccurencesOfRepetition from '../repetition';
         },
         endDate: 1478576134000 + (3 * 24 * 60 * 60 * 1000),
       },
+      day: '2016-11-09',
     },
-  ]);
+  ];
+
+  assert.deepEqual(occurences, expected);
 })();
